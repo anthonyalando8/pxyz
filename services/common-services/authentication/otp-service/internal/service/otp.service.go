38af3db6e84870f565c272cbe7f3cc41aa3e1010
anthonyalando8 <anthonyalando8@gmail.com>
2025-10-19 14:05:48 +0300

    otp service

--- services/common-services/authentication/otp-service/internal/service/otp.service.go
+++ services/common-services/authentication/otp-service/internal/service/otp.service.go
@@ -75,7 +75,7 @@ func (s *OTPService) GenerateOTP(ctx context.Context, userID string, purpose, ch
 	}
 
 	// 3. Save to Redis (live verification)
-	key := fmt.Sprintf("otp:%s:%s", userID, purpose)
+	key := fmt.Sprintf("%s:%s", userID, purpose)
 	err = s.cache.Set(ctx, "otp", key, otp.Code, s.ttl)
 	if err != nil {
 		return err
@@ -123,12 +123,12 @@ func (s *OTPService) createOTP(userID, purpose, channel string) (*repository.OTP
 }
 
 func (s *OTPService) VerifyOTP(ctx context.Context, userID int64, purpose, code string) (bool, error) {
-	key := fmt.Sprintf("otp:%d:%s", userID, purpose)
+	key := fmt.Sprintf("%d:%s", userID, purpose)
 
 	// 1. Get OTP from Redis
 	val, err := s.cache.Get(ctx,"otp", key)
 	if err == redis.Nil {
-		log.Printf("OTP not found or expired | UserID=%d | Purpose=%s", userID, purpose)
+		log.Printf("OTP not found or expired | UserID=%d | Purpose=%s | Key=%s | Code=%s", userID, purpose, key, code)
 		return false, nil // not found (expired or never existed)
 	} else if err != nil {
 		log.Printf("Failed to get OTP from Redis: %v", err)
